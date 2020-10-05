f6e0ccc172ef8392cc554534c685730e73d4fbbb
Luka-dot <79lukask@gmail.com>
2020-10-05 17:27:56 -0400

    image added to sections

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title }) => (
-    <div className='menu-item'>
+const MenuItem = ({ title, imageUrl }) => (
+    <div 
+        className='menu-item' 
+        style={{
+            background: `url(${imageUrl})`
+        }}
+    >
         <div className='content'>
             <h1 className='title'>{ title }</h1>
             <span className='subtitle'>SHOP NOW</span>
