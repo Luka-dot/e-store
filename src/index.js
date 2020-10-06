cb532b279f86a13a7ef5c6420a9bfc5b8b03d2a1
Luka-dot <79lukask@gmail.com>
2020-10-06 19:42:57 -0400

    adding react-router

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link, BrowserRouter
+} from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
