d973890c06ad59d426d532bfa25cb1ce470222f9
Luka-dot <79lukask@gmail.com>
2020-10-07 09:36:04 -0400

    shop page displays all collectiuons previews (no-styling)

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import Directory from './components/directory/directory.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={Directory} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
