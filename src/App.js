704ad5eccf5146994827c8885716bc5340630323
Luka-dot <79lukask@gmail.com>
2020-10-07 16:00:55 -0400

    firebase google authentication added and button styles

--- src/App.js
+++ src/App.js
@@ -6,18 +6,44 @@ import Directory from './components/directory/directory.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header.component/header.component';
 import SignInAndSignOut from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={Directory} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignOut} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log(user)
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path='/' component={Directory} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignOut} />
+        </Switch>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
--- src/components/header.component/header.component.jsx
+++ src/components/header.component/header.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { auth } from '../../firebase/firebase.utils';
+
 import './header.styles.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 
-const Header = () => (
+const Header = ({ currentUser }) => (
     <div className='header' >
         <Link className='logo_container' to="/" >
             <Logo className='logo' />
@@ -12,6 +14,12 @@ const Header = () => (
         <div className='options'>
             <Link className='options' to='/shop'>SHOP</Link>
             <Link className='options' to='/shop'>CONTACT</Link>
+            {
+                currentUser ?
+                (<div className='options' onClick={() => auth.signOut()}>Sign Out</div>)
+                :
+                (<Link className='options' to='/signin' >Sign In</Link>)
+            }
         </div>
     </div>
 )
