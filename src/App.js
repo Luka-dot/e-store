7c6f85e0a1a556583d017463554b93df97c8dd66
Luka-dot <79lukask@gmail.com>
2020-10-10 12:24:49 -0400

    sign-in and sign-out redirect functionality added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
@@ -35,7 +35,7 @@ class App extends React.Component {
           })
         });
       }
-      
+
       setCurrentUser(userAuth)
     });
   }
@@ -59,8 +59,12 @@ class App extends React.Component {
   
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
