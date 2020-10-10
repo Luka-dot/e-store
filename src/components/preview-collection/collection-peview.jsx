c49da50bfc77576de2e25387e1860f24a7286367
Luka-dot <79lukask@gmail.com>
2020-10-10 19:54:18 -0400

    add item to cart functionality added

--- src/components/preview-collection/collection-peview.jsx
+++ src/components/preview-collection/collection-peview.jsx
@@ -10,8 +10,8 @@ const CollectionPreview = ( { title, items } ) => (
             {
                 items
                     .filter((item, index) => index < 4)   // only 4 previews item to be displayed
-                    .map(({ id, ...otherItemProps }) => (
-                        <CollectionItem key={id} {...otherItemProps} ></CollectionItem>
+                    .map((item) => (
+                        <CollectionItem key={item.id} item={item} ></CollectionItem>
                 ))
             }
         </div>
