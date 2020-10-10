46faeee02ab4c1757bf65f9ed47906689d6bc60f
Luka-dot <79lukask@gmail.com>
2020-10-10 17:56:49 -0400

    cart-dropdown component created

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
-import './cart-item.styles.scss';
+import './cart-icon.styles.scss';
 
 const CartIcon = () => (
     <div className='cart-icon'>
