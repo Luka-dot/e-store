a3bbcacf5c8b09fb3c25ca6ea17280c24516d145
Luka-dot <79lukask@gmail.com>
2020-10-11 12:35:47 -0400

    checkout page set up with navigation

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { createStructuredSelector } from 'reselect';
 import './App.css';
 import Directory from './components/directory/directory.component';
 import ShopPage from './pages/shop/shop.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 import Header from './components/header.component/header.component';
 import SignInAndSignOut from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
@@ -53,6 +54,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={Directory} />
           <Route path='/shop' component={ShopPage} />
+          <Route path='/checkout' component={CheckoutPage} />
           <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignOut/>)} />
         </Switch>
       </div>
