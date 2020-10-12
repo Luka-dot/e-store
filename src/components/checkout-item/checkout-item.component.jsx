61de37b500d9dfa6f58e0e7b365d499a48b3ce9a
Luka-dot <79lukask@gmail.com>
2020-10-12 09:11:04 -0400

    minor error in clear item reducer fixed, remove cart-item is set up

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { connect } from 'react-router';
+import { connect } from 'react-redux';
 
 import { clearItemFromCart } from '../../redux/cart/cart.actions';
 
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -21,7 +21,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         case CartActionTypes.CLEAR_ITEM_FROM_CART:
             return {
                 ...state,
-                cartItems: state.cartItems.filter(cartItem => cartItem.id != action.payload.cartItem.id)
+                cartItems: state.cartItems.filter(cartItem => cartItem.id != action.payload.id)
             }
         default:
             return state;
