ed3f0cbf0268d2ce61e7666e5f5b9b1940ab512c
Luka-dot <79lukask@gmail.com>
2020-10-13 16:57:21 -0400

    collection-overview component added

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 
-import CollectionPreview from '../../components/preview-collection/collection-peview';
-
-import { selectCollections } from '../../redux/shop/shop.selector';
+import CollectionOverview from '../../components/collections-overview/collections-overview.component';
 
 const ShopPage = ({ collections }) => (
     <div className='shop-page'>
-        {
-            collections.map(({ id, ...otherCollectionProps }) => (
-                <CollectionPreview key={id} {...otherCollectionProps} />
-            ))
-        }
+        <CollectionOverview />
     </div>
 
 )
@@ -21,4 +13,4 @@ const mapStateToProps = createStructuredSelector({
     collections: selectCollections
 })
 
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
