36a16a086ffd9b12c839882c6605ba82c3655dd0
Luka-dot <79lukask@gmail.com>
2020-10-13 16:25:11 -0400

    shop-component converted to stateless compnenet

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,16 @@
 import React from 'react';
 
-import SHOP_DATA from './shop.data';
 import CollectionPreview from '../../components/preview-collection/collection-peview';
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
+const ShopPage = ({ collections }) => (
+    <div className='shop-page'>
+        {
+            collections.map(({ id, ...otherCollectionProps }) => (
+                <CollectionPreview key={id} {...otherCollectionProps} />
+            ))
+        }
+    </div>
 
-        this.state = {
-            collections: SHOP_DATA
-        };
-    }
-
-    render() {
-        const { collections } = this.state;
-        return (<div className='shop-page'>
-                    {
-                        collections.map(({ id, ...otherCollectionProps }) => (
-                            <CollectionPreview key={id} { ...otherCollectionProps } />
-                        ))
-                    }
-                </div>
-        )
-    }
-}
+)
 
 export default ShopPage;
