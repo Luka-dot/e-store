b435e11142ed00f9e82fbcee888514979c5328c9
Luka-dot <79lukask@gmail.com>
2020-10-17 10:04:44 -0400

    fixed styles on collection page and rout to collections page

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -8,9 +8,11 @@ import { selectCollection } from '../../redux/shop/shop.selector';
 import './collection.styles.scss';
 
 const CollectionPage = ({ collection }) => {
+    console.log(collection)
     const { title, items } = collection;
 
-    <div className='category'>
+    return(
+    <div className='collection-page'>
         <h2 className='title'>{title}</h2>
         <div className='items' >
             {
@@ -20,10 +22,11 @@ const CollectionPage = ({ collection }) => {
             }
         </div>
     </div>
+        )
 };
 
-const mapStateToProps = (state, ownProps) => {
+const mapStateToProps = (state, ownProps) => ({
     collection: selectCollection(ownProps.match.params.collectionId)(state)
-}
+})
 
 export default connect(mapStateToProps)(CollectionPage);
--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -2,7 +2,7 @@ import { createSelector } from 'reselect';
 
 const COLLECTION_ID_MAP = {
     hats: 1,
-    sneekers: 2,
+    sneakers: 2,
     jackets: 3,
     womens: 4,
     mens: 5
