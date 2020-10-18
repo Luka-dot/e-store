a51b55d41a92f377d93e245d7295a13b81fff45d
Luka-dot <79lukask@gmail.com>
2020-10-18 19:47:05 -0400

    deleted local shop data

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -9,7 +9,7 @@ export const selectCollections = createSelector(
 
 export const selectCollectionsForPreview = createSelector(
     [selectCollections],
-    (collections) => Object.keys(collections).map(key => collections[key])
+    (collections) => collections ? Object.keys(collections).map(key => collections[key]) : []
 );
 
 // export const selectCollectionsForPreview = createSelector(
@@ -20,5 +20,5 @@ export const selectCollectionsForPreview = createSelector(
 
 export const selectCollection = collectionUrlParam => createSelector(
     [selectCollections],
-    collections => collections[collectionUrlParam]
+    collections => (collections ? collections[collectionUrlParam] : null)
 );
