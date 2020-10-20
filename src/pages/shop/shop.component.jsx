733ffa917eeadba63a94cd0dd9866652e6ec3640
Luka-dot <79lukask@gmail.com>
2020-10-20 12:15:28 -0400

    connecting new redux thunk actions to shop-component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,45 +1,26 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
 
 import CollectionOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 
-import { updateCollections } from '../../redux/shop/shop.actions';
+import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
 import WithSpinner from '../../components/with-spinner/with-spinner.component';
 
-import { firestore, convertCollectionSnapshotToMap } from '../../firebase/firebase.utils';
 
 const CollectionOverviewWithSpinner = WithSpinner(CollectionOverview);
 const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
-    state = {
-        loading: true
-    };
-
-    unsubscribeFromSnapshot = null;
-
     componentDidMount() {
-        const { updateCollections } = this.props;
-        const collectionRef = firestore.collection('collections');
-
-        // Just an example of the API fetch from Firebase
-        // fetch('https://firestore.googleapis.com/v1/projects/e-store-8ee01/databases/(default)/documents/collections')
-        // .then(resposne => resposne.json())
-        // .then(collections => console.log(collections));
-
-        collectionRef.get().then((async snapshot => {
-            const collectionsMap = convertCollectionSnapshotToMap(snapshot);
-            updateCollections(collectionsMap);
-            this.setState({ loading: false });
-        }))  
+        
 
     };
     
     render() {
         const { match } = this.props;
-        const { loading } = this.state;
         return (
             <div className='shop-page'>
                 <Route exact path={`${match.path}`} render={(props) => <CollectionOverviewWithSpinner isLoading={loading} {...props} /> } />
