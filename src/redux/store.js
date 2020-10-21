3005578ce6ecd3e007c8a269e8ef2926fea88980
Luka-dot <79lukask@gmail.com>
2020-10-21 15:29:53 -0400

    basic saga listener set up and fired for shop compnenet, just a test saga function

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,23 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
-import CreateSageMiddleware from 'redux-saga';
+import createSageMiddleware from 'redux-saga';
+
+import { fetchCollectionsStart } from './shop/shop.sagas';
 
 import rootReducer from './root-reducer';
 
-const middlwares = [thunk];
+const sagaMiddleware = createSageMiddleware();
+
+const middlwares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === 'development') {
     middlwares.push(logger)
 };
 
 export const store = createStore(rootReducer, applyMiddleware(...middlwares));
+
+sagaMiddleware.run(fetchCollectionsStart)
 
 export const persistor = persistStore(store);
 
