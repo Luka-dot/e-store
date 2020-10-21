80bdb940403a09fcc62f388dd837871f07ad8820
Luka-dot <79lukask@gmail.com>
2020-10-21 15:59:25 -0400

    shop collections call moved to sagas

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,9 +1,21 @@
-import { takeEvery,  } from 'redux-saga/effects';
+import { takeEvery, call, put } from 'redux-saga/effects';
+
+import { firestore, convertCollectionSnapshotToMap } from '../../firebase/firebase.utils';
+import { fetchCollectionsSuccess, fetchCollectionsFailure } from './shop.actions';
 
 import ShopActionTypes from './shop.types';
 
 export function* fetchCollectionsAsync() {
-    yield console.log(' fetchCollectionsAsync FIRED!!!')
+    yield console.log(' fetchCollectionsAsync FIRED!!!');
+
+    try {const collectionRef = firestore.collection('collections');
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(convertCollectionSnapshotToMap, snapshot);
+    yield put(fetchCollectionsSuccess(collectionsMap));
+    
+    } catch (error) {
+        yield put(fetchCollectionsFailure(error));
+    };
 }
 
 export function* fetchCollectionsStart() {
