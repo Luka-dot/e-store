9666a50d0d8c0bfba9e877296c2df12bfd05646c
Luka-dot <79lukask@gmail.com>
2020-10-22 17:00:44 -0400

    sign in with email and Google refactor

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ import ShopPage from './pages/shop/shop.component';
 import CheckoutPage from './pages/checkout/checkout.component';
 import Header from './components/header.component/header.component';
 import SignInAndSignOut from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-import { setCurrentUser } from './redux/user/user.actions';
+
 import { selectCurrentUser } from './redux/user/user.selector';
 import HomePage from './pages/homepage/homepage.componenet';
 
@@ -20,8 +19,6 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-
-    const {setCurrentUser} = this.props;
 
     // this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
     //   if (userAuth) {
@@ -65,8 +62,5 @@ const mapStateToProps = createStructuredSelector({
 //  collectionsArray: selectCollectionsForPreview
 })
 
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
-});
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps)(App);
--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,21 +1,21 @@
 import UserActionTypes from './user.types';
 
-export const setCurrentUser = user => ({
-    type: UserActionTypes.SET_CURRENT_USER,
-    payload: user
-});
+// export const setCurrentUser = user => ({
+//     type: UserActionTypes.SET_CURRENT_USER,
+//     payload: user
+// });
 
 export const googleSingInStart = () => ({
     type: UserActionTypes.GOOGLE_SIGN_IN_START
 });
 
-export const googleSignInSuccess = (user) => ({
-    type: UserActionTypes.GOOGLE_SIGN_IN_SUCCESS,
+export const signInSuccess = (user) => ({
+    type: UserActionTypes.SIGN_IN_SUCCESS,
     payload: user
 });
 
-export const googleSingInFailure = (error) => ({
-    type: UserActionTypes.GOOGLE_SIGN_IN_FAILURE,
+export const singInFailure = (error) => ({
+    type: UserActionTypes.SIGN_IN_FAILURE,
     payload: error
 });
 
@@ -24,12 +24,12 @@ export const emailSingInStart = (emailAndPassword) => ({
     payload: emailAndPassword
 });
 
-export const emailSignInSuccess = (user) => ({
-    type: UserActionTypes.EMAIL_SIGN_IN_SUCCESS,
-    payload: user
-});
+// export const emailSignInSuccess = (user) => ({
+//     type: UserActionTypes.SIGN_IN_SUCCESS,
+//     payload: user
+// });
 
-export const emailSignInFaiure = (error) => ({
-    type: UserActionTypes.EMAIL_SIGN_IN_FAILURE,
-    payload: error
-});
+// export const emailSignInFaiure = (error) => ({
+//     type: UserActionTypes.SIGN_IN_FAILURE,
+//     payload: error
+// });
