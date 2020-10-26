0ae247f191a1ec3926092c4c664bdce49c75d65a
Luka-dot <79lukask@gmail.com>
2020-10-26 19:35:15 -0400

    clear cart Saga working and invoked when loggimg out

--- src/redux/cart/cart.sagas.js
+++ src/redux/cart/cart.sagas.js
@@ -8,9 +8,9 @@ export function* clearCartOnSignOut() {
 };
 
 export function* onSignOutSuccess() {
-    yield takeLatest(UserActionTypes.SIGN_OUT_SUCCESS, clearCart)
+    yield takeLatest(UserActionTypes.SIGN_OUT_SUCCESS, clearCartOnSignOut)
 };
 
 export function* cartSagas() {
-    yield(all[])
+    yield all([call(onSignOutSuccess)])
 };
