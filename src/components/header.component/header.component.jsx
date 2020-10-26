3a950fe83daa315b1215ae5f121b40661f37e64e
Luka-dot <79lukask@gmail.com>
2020-10-26 17:42:13 -0400

    sign out starts added to header component

--- src/components/header.component/header.component.jsx
+++ src/components/header.component/header.component.jsx
@@ -7,12 +7,13 @@ import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selector';
+import { signOutStart } from '../../redux/user/user.actions';
 
 // import './header.styles.scss';
 import { HeaderContainer, LogoContainer, OptionsContainer, OptionDiv, OptionLink } from './header.styles';
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 
-const Header = ({ currentUser, hidden }) => (
+const Header = ({ currentUser, hidden, signOutStart }) => (
     <HeaderContainer >
         <LogoContainer to="/" >
             <Logo className='logo' />
@@ -22,7 +23,7 @@ const Header = ({ currentUser, hidden }) => (
             <OptionLink to='/shop'>CONTACT</OptionLink>
             {
                 currentUser ?
-                (<OptionDiv onClick={() => auth.signOut()}>Sign Out</OptionDiv>)
+                (<OptionDiv onClick={signOutStart}>Sign Out</OptionDiv>)
                 :
                 (<OptionLink to='/signin' >Sign In</OptionLink>)
             }
@@ -41,4 +42,8 @@ const mapStateToProps = createStructuredSelector({
 
 });
 
-export default connect(mapStateToProps)(Header);
+const mapDispatchToProps = dispatch => ({
+    signOutStart: ()=> dispatch(signOutStart)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -6,15 +6,20 @@ const INITIAL_STATE = {
 
 const userReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        case UserActionTypes.GOOGLE_SIGN_IN_SUCCESS:   // stacking cases -> if either is sucess  return will trigger
-        case UserActionTypes.EMAIL_SIGN_IN_SUCCESS:
+        case UserActionTypes.SIGN_IN_SUCCESS:   
             return {
                 ...state,
                 currentUser: action.payload,
                 error: null
             };
-        case UserActionTypes.GOOGLE_SIGN_IN_FAILURE:
-        case UserActionTypes.EMAIL_SIGN_FAILURE:
+        case UserActionTypes.SIGN_OUT_SUCCESS:
+            return {
+                ...state,
+                currentUser: null,
+                error: null
+            }
+        case UserActionTypes.SIGN_IN_FAILURE:    // stacking cases -> if either is sucess  return will trigger
+        case UserActionTypes.SIGN_OUT_FAILURE:
             return {
                 ...state,
                 error: action.payload
