8f84042ac31bd329bc025b8db2f5a6686a108dec
Luka-dot <79lukask@gmail.com>
2020-10-27 08:44:29 -0400

    sign-up reducer modify

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -20,6 +20,7 @@ const userReducer = (state = INITIAL_STATE, action) => {
             }
         case UserActionTypes.SIGN_IN_FAILURE:    // stacking cases -> if either is sucess  return will trigger
         case UserActionTypes.SIGN_OUT_FAILURE:
+        case UserActionTypes.SIGN_UP_FAILURE:
             return {
                 ...state,
                 error: action.payload
