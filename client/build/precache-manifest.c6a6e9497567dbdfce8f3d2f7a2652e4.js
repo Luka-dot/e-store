db02a5eb9ece30298644dc492428399068637469
Luka-dot <79lukask@gmail.com>
2020-10-29 22:35:04 -0400

    fixing versions

---
+++ client/build/precache-manifest.c6a6e9497567dbdfce8f3d2f7a2652e4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4912c4a8290201996c917474406e0014",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d30c62259f9496473357",
+    "url": "/static/css/main.13740976.chunk.css"
+  },
+  {
+    "revision": "ac841da17d2c4eebcbfa",
+    "url": "/static/js/2.5d5016f9.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/static/js/2.5d5016f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d30c62259f9496473357",
+    "url": "/static/js/main.f038acb2.chunk.js"
+  },
+  {
+    "revision": "034146c7c8de99391846",
+    "url": "/static/js/runtime-main.c60471ea.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
