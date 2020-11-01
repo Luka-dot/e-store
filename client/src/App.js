7669c7bcb920c94cf52be380f49bb31b993ea942
Luka-dot <79lukask@gmail.com>
2020-11-01 12:07:50 -0500

    react lazy and suspense added

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import { GlobalStyle } from './global.styles';
 
-import ShopPage from './pages/shop/shop.component';
-import CheckoutPage from './pages/checkout/checkout.component';
+//  import ShopPage from './pages/shop/shop.component';
+//  import CheckoutPage from './pages/checkout/checkout.component';
 import Header from './components/header.component/header.component';
-import SignInAndSignOut from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+//  import SignInAndSignOut from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { checkUserSession } from './redux/user/user.actions';
 
 import { selectCurrentUser } from './redux/user/user.selector';
-import HomePage from './pages/homepage/homepage.componenet';
+//  import HomePage from './pages/homepage/homepage.componenet';
+
+const HomePage = lazy(() => import('./pages/homepage/homepage.componenet'));
+const ShopPage = lazy(() => import('./pages/shop/shop.component'));
+const CheckoutPage = lazy(() => import('./pages/checkout/checkout.component'));
+const SignInAndSignOut = lazy(() => import('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component'));
 
 // import { selectCollectionsForPreview } from './redux/shop/shop.selector';
 
@@ -26,10 +31,12 @@ const App = ({ checkUserSession, currentUser }) => {
         <GlobalStyle />
         <Header />
         <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-          <Route path='/checkout' component={CheckoutPage} />
-          <Route exact path='/signin' render={() => currentUser ? (<Redirect to='/' />) : (<SignInAndSignOut/>)} />
+          <Suspense fallback={<div>... Loading ...</div>}>
+            <Route exact path='/' component={HomePage} />
+            <Route path='/shop' component={ShopPage} />
+            <Route path='/checkout' component={CheckoutPage} />
+            <Route exact path='/signin' render={() => currentUser ? (<Redirect to='/' />) : (<SignInAndSignOut/>)} />
+          </Suspense>
         </Switch>
       </div>
     );
